8a155609172332e329314a233a2c9be70deb7f6b
Omar Aglan <Omar.aglan91@gmail.com>
2024-07-12 14:40:31 +0300

    update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,8 @@ const swiper = new Swiper('.tinyflow-slider', {
 	effect: 'fade',
 	loop: true,
 	autoplay: {
-		disableOnInteraction: false
+		disableOnInteraction: false,
+		pauseOnMouseEnter: false
 	},
 	keyboard: {
 		enabled: true,
@@ -46,4 +47,25 @@ const swiper = new Swiper('.tinyflow-slider', {
 			);
 		}
 	}
+});
+//nav
+document.addEventListener('DOMContentLoaded', () => {
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(
+		document.querySelectorAll('.navbar-burger'),
+		0
+	);
+
+	// Add a click event on each of them
+	$navbarBurgers.forEach((el) => {
+		el.addEventListener('click', () => {
+			// Get the target from the "data-target" attribute
+			const target = el.dataset.target;
+			const $target = document.getElementById(target);
+
+			// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+			el.classList.toggle('is-active');
+			$target.classList.toggle('is-active');
+		});
+	});
 });
